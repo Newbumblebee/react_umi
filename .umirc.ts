d2e0b63973587c7557bf45575c684d5f6ed360dd
zj <872157062@qq.com>
2022-01-21 15:38:11 +0800

    1

--- .umirc.ts
+++ .umirc.ts
@@ -1,3 +1,5 @@
+//配置文件，包含 umi 内置功能和插件的配置。
+
 import { defineConfig } from 'umi';
 
 export default defineConfig({
@@ -5,6 +7,9 @@ export default defineConfig({
     type: 'none',
   },
   layout: {},
-  routes: [{ path: '/', component: '@/pages/index' }],
+  routes: [
+    //{ path: '/', component: '@/pages/index' },
+    { path: '/', component: '@/pages/ant/index' },
+  ],
   fastRefresh: {},
 });
