bf7c32443a526223699f29d82452da3a60a1e54d
zj <zj@Newgrand.cn>
2022-07-01 17:07:08 +0800

    1

--- config/routes.ts
+++ config/routes.ts
@@ -1,9 +1,29 @@
 // config/routes.ts
 
 export default [
-  { exact: true, path: '/', component: 'index' },
-  { path: '/usr', exact: true, component: '@/pages/index' },
-  { path: '/lodash', exact: true, component: '@/pages/lodash' },
-  { path: '/lodash01', exact: true, component: '@/pages/lodash01' },
-  { path: '/sty', exact: true, component: '@/pages/study/Parent' },
+  {
+    flatMenu: true,
+    routes: [
+      { exact: true, name: '首页', path: '/', component: 'index' },
+      { path: '/usr', exact: true, name: '用户', component: '@/pages/index' },
+      {
+        path: '/lodash',
+        exact: true,
+        name: 'lodash',
+        component: '@/pages/lodash',
+      },
+      {
+        path: '/lodash01',
+        exact: true,
+        name: 'lodash01',
+        component: '@/pages/lodash01',
+      },
+      {
+        path: '/sty',
+        exact: true,
+        name: 'sty',
+        component: '@/pages/study/Parent',
+      },
+    ],
+  },
 ];
